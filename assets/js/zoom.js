a936c5e0124f863d46f02f912442bc37c99cc698
desword <desword@zju.edu.cn>
2024-09-11 09:15:46 +0000

    Deploying to gh-pages from @ desword/desword.github.io@3d6695309c7ce444ce50595de4c630b40b1f7a2f 🚀

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){medium_zoom=mediumZoom("[data-zoomable]",{background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee"})});
+$(document).ready(function(){medium_zoom=mediumZoom("[data-zoomable]",{margin:24,background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee",scrollOffset:0})});
