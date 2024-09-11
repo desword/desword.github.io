3d6695309c7ce444ce50595de4c630b40b1f7a2f
garinchen <desword@zju.edu.cn>
2024-09-11 17:11:28 +0800

    update

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,6 +1,8 @@
 // Initialize medium zoom.
 $(document).ready(function () {
   medium_zoom = mediumZoom("[data-zoomable]", {
-    background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for trasparency.
+    margin: 24, // Adjust margin as needed
+    background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for transparency.
+    scrollOffset: 0 // Adjust scroll offset if needed
   });
 });
